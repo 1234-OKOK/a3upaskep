75050ddf4763e8646a8e822f05a7cedbee552afb
SupaSkep <kevindasupa@gmail.com>
2023-12-05 01:10:11 +0630

    init

---
+++ a3u_GAYSEX/addons/core/Includes/mod_name.hpp
@@ -0,0 +1,4 @@
+#define MODFOLDER a3u_GAYSEX
+#ifndef PREFIX
+    #define PREFIX a3u_GAYSEX
+#endif
---
+++ a3u_GAYSEX/addons/templates/script_component.hpp
@@ -0,0 +1,2 @@
+#define COMPONENT templates
+#include "\x\a3u_GAYSEX\addons\core\Includes\script_mod.hpp"
