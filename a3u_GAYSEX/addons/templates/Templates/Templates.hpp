f77787e9ef92c19f0f56f24d163cc8a4e496865e
SupaSkep <kevindasupa@gmail.com>
2023-12-13 19:11:45 +0630

    SupaSkep FIXED it

--- a3u_GAYSEX/addons/templates/Templates/Templates.hpp
+++ a3u_GAYSEX/addons/templates/Templates/Templates.hpp
@@ -3,10 +3,36 @@ class Templates {
     class 3CBF_OFN : 3CBF_Base
     {
         side = "Reb";
-        flagTexture = "a3\data_f\flags\flag_fia_co.paa";
+        flagTexture = "a3\data_f\flags\flag_us_co.paa";
         name = "THE UNITED STATES ARMY"; 
         basepath = QPATHTOFOLDER(Templates\SupaSkep); //the path to the template folder
         file = "USA_reb";
         description = "One of the militaries of all time";
     };
+    class 3CBF_ESP : 3CBF_Base
+    {
+        side = "Occ";
+        flagTexture = "a3\data_f\flags\flag_fia_co.paa";
+        name = "THE EINSHEITPAKT"; 
+        basepath = QPATHTOFOLDER(Templates\SupaSkep); //the path to the template folder
+        file = "ESP_occ";
+        description = "Speerhead";
+    };
+    class 3CBF_CUB : 3CBF_Base
+    {
+        side = "Riv";
+        flagTexture = "a3\data_f\flags\flag_fia_co.paa";
+        name = "CUBAN SABOTEURS"; 
+        basepath = QPATHTOFOLDER(Templates\SupaSkep); //the path to the template folder
+        file = "CUB_Riv";
+        description = "Viva La Revolution";
+    };
+     class 3CBF_GRE : 3CBF_Base
+    {
+        side = "occ";
+        flagTexture = "a3\data_f\flags\flag_fia_co.paa";
+        name = "GRENADIAN ARMED FORCES"; 
+        basepath = QPATHTOFOLDER(Templates\SupaSkep); //the path to the template folder
+        file = "GRE_occ";
+        description = "Viva La Revolution";
 };
